694df6046c05ee4c3ed4871072528f9efd74b426
Lara1255 <colegiolaraa@gmail.com>
2025-09-01 16:48:32 -0300

    saque el estilo

---
+++ componentes/conexion.php
@@ -0,0 +1,13 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$contrasenia = "";
+$base = "m_viajes";
+
+//conexion propia a la bbdd
+$conexion = new mysqli($servidor,$usuario,$contrasenia,$base);
+
+// chequear la conexion
+if($conexion->connect_error){
+    die("Error de conexion: ". $conexion->connect_errror);
+}
